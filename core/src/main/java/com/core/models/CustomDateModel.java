0ffe8a6c3e52ddb99fb69788e08d38c14fa200f9
agarwalabhinav <abhinav141154cse@gmail.com>
2018-04-05 15:23:57 +0530

    date component with inject and via

--- core/src/main/java/com/core/models/CustomDateModel.java
+++ core/src/main/java/com/core/models/CustomDateModel.java
@@ -9,53 +9,67 @@ import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables= {Resource.class, SlingHttpServletRequest.class} )
+@Model(adaptables = { Resource.class, SlingHttpServletRequest.class })
 public class CustomDateModel {
 
 	@Inject
 	@Via("resource")
 	private String headerDate;
 	
+	
+	@ValueMapValue
+	@Named("jcr:created")
+	private  java.util.Calendar date;
+
+	public String getDate1() {
+		return date.getTime().toString();
+	}
+
 	public List<String> getDate() throws ParseException {
 		
+	//	return headerDate;
+				
+		
+
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 		Date date = df.parse(headerDate);
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
-		
+
 		String result;
-		
+
 		SimpleDateFormat formatDay = new SimpleDateFormat("EEEE");
 		formatDay.setCalendar(cal);
-	    String dayFormatted;
-	    dayFormatted = formatDay.format(cal.getTime());
-	    
-	    SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
-	    formatDate.setCalendar(cal);
-	    String dateFormatted;
-	    dateFormatted = formatDate.format(cal.getTime());
-	    
-	    List<String> al = new ArrayList<String>();
-		result="date: "+dateFormatted+", Day: "+dayFormatted;
-		al.add(result);
 		
-		for(int i=0;i<5;i++) {
+		String dayFormatted;
+		dayFormatted = formatDay.format(cal.getTime());
+
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+		formatDate.setCalendar(cal);
+		String dateFormatted;
+		dateFormatted = formatDate.format(cal.getTime());
+
+		List<String> al = new ArrayList<String>();
+		result = "date: " + dateFormatted + ", Day: " + dayFormatted;
+		al.add(result);
+
+		for (int i = 0; i < 5; i++) {
 			cal.add(Calendar.DATE, 7);
-			
 			formatDate.setCalendar(cal);
 			dateFormatted = formatDate.format(cal.getTime());
-			
-			result="next "+dayFormatted+" : "+dateFormatted;
+			result = "next " + dayFormatted + " : " + dateFormatted;
 			al.add(result);
 		}
 		return al;
 	}
-	
-	
+
 }
--- core/src/main/java/com/core/models/CustomDateWCM.java
+++ core/src/main/java/com/core/models/CustomDateWCM.java
@@ -1,32 +1,54 @@
 package com.core.models;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.jcr.Node;
 
 import org.apache.sling.api.resource.Resource;
 
-import com.adobe.cq.sightly.WCMUse;
+import com.adobe.cq.sightly.WCMUsePojo;
 
-public class CustomDateWCM extends WCMUse{
+public class CustomDateWCM extends WCMUsePojo {
 
-	private  String dateFormatted;
-	
+	private List<String> al = new ArrayList<String>();
+
 	@Override
 	public void activate() throws Exception {
 		// TODO Auto-generated method stub
 		Resource resource = getResource();
-		 Node node = resource.adaptTo(Node.class);
-	       Calendar p = node.getProperty("headerDate").getValue().getDate();
-	      // return p.toString();
-	       SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
-		    fmt.setCalendar(p);
-		    dateFormatted = fmt.format(p.getTime());	
+		Node node = resource.adaptTo(Node.class);
+
+		Calendar p = node.getProperty("headerDate").getValue().getDate();
+
+		String dateFormatted;
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+		formatDate.setCalendar(p);
+		dateFormatted = formatDate.format(p.getTime());
+
+		String dayFormatted;
+		SimpleDateFormat formatDay = new SimpleDateFormat("EEEE");
+		formatDay.setCalendar(p);
+		dayFormatted = formatDay.format(p.getTime());
+
+		String result;
+		result = "date: " + dateFormatted + ", Day: " + dayFormatted;
+		al.add(result);
+		for (int i = 0; i < 5; i++) {
+			p.add(Calendar.DATE, 7);
+
+			formatDate.setCalendar(p);
+			dateFormatted = formatDate.format(p.getTime());
+
+			result = "next " + dayFormatted + " : " + dateFormatted;
+			al.add(result);
+		}
 	}
-	
-	public String getDate() {
-		return dateFormatted;
+
+	public List<String> getDate() {
+		return al;
 	}
 
 }
